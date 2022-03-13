ce6d626a50e9f3af4e3119e40287022e3a45582c
MR-HENDRA <hunterblackshadow26@gmail.com>
2022-03-13 20:52:49 +0800

    SDA 2022

---
+++ SDA/OperatorAritmatika.py
@@ -0,0 +1,21 @@
+nilai1 = 10
+nilai2 = 5
+
+#PENJUMLAHAN
+print("Hasil Operasi: ", nilai1+nilai2)
+#PENGURANGAN
+print("Hasil Operasi: ", nilai1-nilai2)
+#PERKALIAN
+print("Hasil Operasi: ", nilai1*nilai2)
+#PEMBAGIAN
+print("Hasil Operasi: ", nilai1/nilai2)
+#SISA BAGI
+print("Hasil Operasi: ", nilai1%nilai2)
+#PANGKAT
+print("Hasil Operasi: ", nilai1**nilai2)
+#PEMBAGIAN BULAT
+print("Hasil Operasi: ", nilai1//nilai2)
+
+
+
+

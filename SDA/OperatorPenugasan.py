ded043b8797f77ba746630fc3848667f2d6ab51b
MR-HENDRA <hunterblackshadow26@gmail.com>
2022-03-13 21:44:39 +0800

    Operator Penugasan

---
+++ SDA/OperatorPenugasan.py
@@ -0,0 +1,14 @@
+nilai = 10
+print(nilai)
+nilai += 5
+print(nilai)
+nilai -= 3
+print(nilai)
+nilai *= 2
+print(nilai)
+nilai /= 4
+print(nilai)
+nilai **= 2
+print(nilai)
+nilai  //= 6
+print(nilai)

fe69a769fe755e24cd688ff4464fae8b08e1238b
MR-HENDRA <hunterblackshadow26@gmail.com>
2022-03-19 23:29:28 +0800

    Full Tuple

---
+++ SDA/Collection.py
@@ -0,0 +1,13 @@
+# Isilah dengan beberapa value/nilai, kemudian tampilkan menggunakan perulangan.
+# Cobalah update salah satu value/nilai dari Collections tersebut.
+# Coba hapus salah satu value dari  Collections.
+# Coba tambahkan value ke Collections tersebut.
+
+random = ["Hendra Usman", 18, 170.5, True]
+for i in random:
+    print(i)
+i = 0
+while i < 1:
+    print(random)
+    
+    i += 1

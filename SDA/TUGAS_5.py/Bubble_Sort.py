a53558b0cc559f4db6cf6f085dd52591c91907b6
MR-HENDRA <hunterblackshadow26@gmail.com>
2022-04-13 11:22:47 +0800

    Bubble Sort

---
+++ SDA/TUGAS_5.py/Bubble_Sort.py
@@ -0,0 +1,15 @@
+# Bubble Sort
+
+database=[21,34,56,78,90,12,45,67,89,11,33]
+print(f"Sebelum diurutkan : {database}\n")
+print(f"Banyak data : {len(database)}\n")
+
+def bubbleSort(data):
+    for i in range(len(data)-1,0,-1):
+        print("langkah")
+        for j in range(0, len(data)-1):
+            if data[j] > data[j+1]:
+                data[j], data[j+1] = data[j+1], data[j]
+        print(data)
+        
+bubbleSort(database)
